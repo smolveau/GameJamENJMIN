fab960dbf61904aa6fe9e66f27bfa42439c6cc15
Youé Graillot <youe.graillot@gmail.com>
2019-01-26 18:10:59 +0100

    + Controller

---
+++ Assets/Scripts/UnityEventsCustom.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Events;
+using UnityEngine;
+
+[System.Serializable]
+public class UnityEventInt : UnityEvent<int> {}
+
+[System.Serializable]
+public class UnityEventBool : UnityEvent<bool> {}
+
+[System.Serializable]
+public class UnityEventFloat : UnityEvent<float> {}
+
+[System.Serializable]
+public class UnityEventVector2 : UnityEvent<Vector2> {}
+
+[System.Serializable]
+public class UnityEventDouble : UnityEvent<double> {}
+
+[System.Serializable]
+public class UnityEventChar : UnityEvent<char> {}
+
+[System.Serializable]
+public class UnityEventString : UnityEvent<string> {}
+
+[System.Serializable]
+public delegate void VoidDelegate();
