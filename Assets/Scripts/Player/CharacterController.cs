726d07d90ae99c1b95bf02938f53be49dac99a42
Youé Graillot <youe.graillot@gmail.com>
2019-01-26 22:25:33 +0100

    + bourré

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -13,16 +13,10 @@ public class CharacterController : MonoBehaviour
 
 
     const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-    private bool m_Grounded;            // Whether or not the player is grounded.
     const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
     private Rigidbody2D m_Rigidbody2D;
     private bool m_FacingRight = true;  // For determining which way the player is currently facing.
     private Vector3 m_Velocity = Vector3.zero;
-
-    [Header("Events")]
-    [Space]
-
-    public UnityEvent OnLandEvent;
 
     [System.Serializable]
     public class BoolEvent : UnityEvent<bool> { }
@@ -30,34 +24,6 @@ public class CharacterController : MonoBehaviour
     private void Awake()
     {
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
-
-        if (OnLandEvent == null)
-            OnLandEvent = new UnityEvent();
-    }
-
-    private void FixedUpdate()
-    {
-        bool wasGrounded = m_Grounded;
-        m_Grounded = false;
-
-        // The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-        // This can be done using layers instead but Sample Assets will not overwrite your project settings.
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-        for (int i = 0; i < colliders.Length; i++)
-        {
-            if (colliders[i].gameObject != gameObject)
-            {
-                m_Grounded = true;
-                if (!wasGrounded)
-                    OnLandEvent.Invoke();
-            }
-        }
-    }
-
-    public void Jump()
-    {
-        if (m_Grounded && enabled)
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
     }
 
     public void Move(float move, bool crouch, bool jump)
@@ -81,10 +47,8 @@ public class CharacterController : MonoBehaviour
         }
 
         // If the player should jump...
-        if (m_Grounded && jump)
+        if (jump)
         {
-            // Add a vertical force to the player.
-            m_Grounded = false;
             jump = false;
             m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
         }
@@ -93,7 +57,7 @@ public class CharacterController : MonoBehaviour
     public void Move(Vector2 input)
     {
         if(enabled)
-            m_Rigidbody2D.AddForce(Vector2.right * input * m_Speed);
+            m_Rigidbody2D.AddForce(Camera.main.transform.rotation * input * m_Speed);
     }
 
 
