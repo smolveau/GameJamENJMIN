40a03339d5e80fd497c1a3f73854b7fe4dccc6a7
Youé Graillot <youe.graillot@gmail.com>
2019-01-27 14:17:10 +0100

    End Menu

---
+++ Assets/Scripts/ColorHue.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColorHue : MonoBehaviour
+{
+    Image m_image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_image = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_image.color = Color.HSVToRGB(Mathf.PingPong(Time.time / 2, 1) , 1, 1);
+    }
+}
